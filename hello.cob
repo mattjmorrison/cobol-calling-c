660ef0ee467f1923b81e880735672c863265780a
mattjmorrison <mattj.morrison@gmail.com>
2012-05-01 13:37:39 -0500

    added readme

--- hello.cob
+++ hello.cob
@@ -3,10 +3,10 @@
         ENVIRONMENT DIVISION.
         DATA DIVISION.
         WORKING-STORAGE SECTION.
-        01 C-STRING-POINTER PIC S9(9) BINARY.
+        01 C-STRING-POINTER PIC S9(9) COMP.
         01 HELLO-WORLD.
-                03 HELLO PIC X(6) VALUE "Hello ".
-                03 WORLD PIC X(6) VALUE "World!".
+                03 HELLO PIC X(6) VALUE "HELLO ".
+                03 WORLD PIC X(6) VALUE "WORLD".
         01 PIPE   PIC X VALUE '|'.
         01 PRINT-RESULT PIC X(150) VALUE SPACES.
         LINKAGE SECTION.
@@ -14,23 +14,27 @@
                 03 RESULT    PIC X(200).
         PROCEDURE DIVISION.
 
-            DISPLAY "STARTING COBOL".
-
-            DISPLAY "CALLING C FROM COBOL".
-            CALL "say"
+            DISPLAY '*********************************'.
+            DISPLAY "*         STARTING COBOL        *".
+            DISPLAY "*      CALLING C FROM COBOL     *".
+            DISPLAY '*********************************'.
+            CALL "getstring"
                 USING HELLO-WORLD
                 RETURNING C-STRING-POINTER.
-            DISPLAY 'BACK IN COBOL AFTER CALLING C'
 
-            DISPLAY C-STRING-POINTER.
-            DISPLAY HELLO-WORLD.
-      *     SET ADDRESS OF LINKAGE-DATA TO C-STRING-POINTER.            
+            DISPLAY '*********************************'.
+            DISPLAY '* BACK IN COBOL AFTER CALLING C *'.
+            DISPLAY '*          ', C-STRING-POINTER, "           *".
+            DISPLAY '*          ', HELLO-WORLD, '         *'.
+            DISPLAY '*********************************'.
+      *      SET ADDRESS OF LINKAGE-DATA TO C-STRING-POINTER.            
 
       *      STRING 
       *          PIPE DELIMITED BY SIZE
       *          RESULT DELIMITED BY SIZE,
       *          PIPE DELIMITED BY SIZE
       *          INTO PRINT-RESULT.
-            DISPLAY PRINT-RESULT.
-            DISPLAY "LEAVING COBOL".
+            DISPLAY '*********************************'.
+            DISPLAY "*         LEAVING COBOL         *".
+            DISPLAY '*********************************'.
             STOP RUN.
--- say.c
+++ say.c
@@ -2,15 +2,24 @@
 //#include <Python.h>
 
 char*
-say(char *hello, char *world)
+getstring(char *hello, char *world)
 {
     printf("Starting C\n");
     printf("%s\n", hello, world);
     //Py_Initialize();
+    //mod = Py_Import("mod")
+    //Py_GetAttr(mod, 'somefunc')
+    // Py_CallFunc(mod, func, args, kwargs)
     //PyRun_SimpleString("from time import time, ctime\n"
     //                   "print 'Today is', ctime(time())\n")
     //Py_Finalize();
     printf("Leaving C\n");
     char *x = "These are some chars";
     return x;
+}
+
+int
+getint(char *hello, char *world){
+    printf("%s\n", hello, world);
+    return 12345;
 }
